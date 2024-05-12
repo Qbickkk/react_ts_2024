6fbb71498096ce0f0df5b74e801dcda6613b6960
shulika <illia.shulika@gmail.com>
2024-05-13 00:07:50 +0300

    lesson2 state lifting

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,22 @@
+import {UsersComponent} from "./components/UsersComponent";
+import {useState} from "react";
+
+
 const App = () => {
+
+        const [userr, setUserr] = useState<{name: string, age:number, status: boolean}>()
+
+    const lift = (user:any) => {
+        setUserr(user)
+    }
+
     return (
-        <div>
-            App
-        </div>
+        <>
+            <UsersComponent liftProp={lift}/>
+            <hr/>
+            {userr?.name}
+            <hr/>
+        </>
     );
 };
 
