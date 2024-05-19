927d3110fe21d10db032f5a10dc20018c6464345
shulika <illia.shulika@gmail.com>
2024-05-19 13:05:44 +0300

    hooks useRef

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
+import {useRef, useState} from "react";
+
 const App = () => {
+
+    const [x, setX] = useState<number>(0);
+
+    const y = useRef(0);
+
+    console.log('work')
+
     return (
         <div>
-             App
+             <button onClick={()=>{
+                setX((x)=>x + 1)
+             }}>change x {x}</button>
+            <button onClick={()=>{
+                y.current ++;
+                console.log(y)
+             }}>change y {y.current}</button>
         </div>
     );
 };
