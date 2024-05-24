613d0445d4b9457a79fb8c17da43bc675d8747cb
shulika <illia.shulika@gmail.com>
2024-05-25 00:19:55 +0300

    lesson5 controller components

---
+++ src/components/FormComponent.tsx
@@ -0,0 +1,38 @@
+import {FC, FormEvent, useState} from "react";
+
+interface IFormProps {
+    username: string
+    password: string
+}
+
+const FormComponent: FC = () => {
+
+    const [formState, setFormState] = useState<IFormProps>({
+        username: 'default',
+        password: 'default'
+    })
+
+    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        let user = {
+            username: formState.username,
+            password: formState.password
+        };
+        console.log(user)
+    };
+
+    const handleChange = (e: FormEvent<HTMLInputElement>) => {
+        let input = e.currentTarget;
+        setFormState({...formState, [input.name]: input.value});
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input type="text" name={'username'} value={formState.username} onChange={handleChange}/>
+            <input type="text" name={'password'} value={formState.password} onChange={handleChange}/>
+            <button>save</button>
+        </form>
+    );
+};
+
+export {FormComponent};
